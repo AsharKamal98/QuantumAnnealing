c1f1ffdd4c1ab70a4331b6efceab23ae7793553c
Ashar Kamal <ashar.kamal98@gmail.com>
2023-08-15 11:57:47 +0200

    Updated README. Added Include paths text file

---
+++ IncludePaths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#export CPLUS_INCLUDE_PATH='/home/akamal/QuantumAnnealing/eigen-3.4.0:/home/akamal/QuantumAnnealing/boost_1_82_0'
+export CPLUS_INCLUDE_PATH='eigen-3.4.0:boost_1_82_0'
+
